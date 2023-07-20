6a3b05acddef9a7be9776f3ba43ffd3e3290b7b6
Abhishek Choudhary <shreemaan.abhishek@gmail.com>
2023-07-20 15:43:07 +0545

    add script for m1 build

---
+++ build-on-macos.sh
@@ -0,0 +1,173 @@
+#!/usr/bin/env bash
+set -euo pipefail
+set -x
+
+
+export openssl_prefix=/opt/homebrew/Cellar/openssl@1.1/1.1.1t/
+export zlib_prefix=/opt/homebrew/Cellar/zlib/
+export pcre_prefix=/opt/homebrew/Cellar/pcre/8.45
+
+export cc_opt="-DNGX_LUA_ABORT_AT_PANIC -I${zlib_prefix}/include -I${pcre_prefix}/include -I${openssl_prefix}/include"
+export ld_opt="-L${zlib_prefix}/lib -L${pcre_prefix}/lib -L${openssl_prefix}/lib -Wl,-rpath,${zlib_prefix}/lib:${pcre_prefix}/lib:${openssl_prefix}/lib"
+
+
+version=${version:-0.0.0}
+
+if ([ $# -gt 0 ] && [ "$1" == "latest" ]) || [ "$version" == "latest" ]; then
+    ngx_multi_upstream_module_ver="master"
+    mod_dubbo_ver="master"
+    apisix_nginx_module_ver="main"
+    wasm_nginx_module_ver="main"
+    lua_var_nginx_module_ver="master"
+    grpc_client_nginx_module_ver="main"
+    amesh_ver="main"
+    debug_args="--with-debug"
+    OR_PREFIX=${OR_PREFIX:="/opt/homebrew/bin/openresty-debug"}
+else
+    ngx_multi_upstream_module_ver="1.1.1"
+    mod_dubbo_ver="1.0.2"
+    apisix_nginx_module_ver="1.12.0"
+    wasm_nginx_module_ver="0.6.4"
+    lua_var_nginx_module_ver="v0.5.3"
+    grpc_client_nginx_module_ver="v0.4.0"
+    amesh_ver="main"
+    debug_args=${debug_args:-}
+    OR_PREFIX=${OR_PREFIX:="/opt/homebrew/bin/openresty"}
+fi
+
+prev_workdir="$PWD"
+repo=$(basename "$prev_workdir")
+workdir=$(mktemp -d)
+cd "$workdir" || exit 1
+
+or_ver="1.21.4.1"
+wget --no-check-certificate https://openresty.org/download/openresty-${or_ver}.tar.gz
+tar -zxvpf openresty-${or_ver}.tar.gz > /dev/null
+
+if [ "$repo" == ngx_multi_upstream_module ]; then
+    cp -r "$prev_workdir" ./ngx_multi_upstream_module-${ngx_multi_upstream_module_ver}
+else
+    git clone --depth=1 -b $ngx_multi_upstream_module_ver \
+        https://github.com/api7/ngx_multi_upstream_module.git \
+        ngx_multi_upstream_module-${ngx_multi_upstream_module_ver}
+fi
+
+if [ "$repo" == mod_dubbo ]; then
+    cp -r "$prev_workdir" ./mod_dubbo-${mod_dubbo_ver}
+else
+    git clone --depth=1 -b $mod_dubbo_ver \
+        https://github.com/api7/mod_dubbo.git \
+        mod_dubbo-${mod_dubbo_ver}
+fi
+
+if [ "$repo" == apisix-nginx-module ]; then
+    cp -r "$prev_workdir" ./apisix-nginx-module-${apisix_nginx_module_ver}
+else
+    git clone --depth=1 -b $apisix_nginx_module_ver \
+        https://github.com/api7/apisix-nginx-module.git \
+        apisix-nginx-module-${apisix_nginx_module_ver}
+fi
+
+if [ "$repo" == wasm-nginx-module ]; then
+    cp -r "$prev_workdir" ./wasm-nginx-module-${wasm_nginx_module_ver}
+else
+    git clone --depth=1 -b $wasm_nginx_module_ver \
+        https://github.com/api7/wasm-nginx-module.git \
+        wasm-nginx-module-${wasm_nginx_module_ver}
+fi
+
+if [ "$repo" == lua-var-nginx-module ]; then
+    cp -r "$prev_workdir" ./lua-var-nginx-module-${lua_var_nginx_module_ver}
+else
+    git clone --depth=1 -b $lua_var_nginx_module_ver \
+        https://github.com/api7/lua-var-nginx-module \
+        lua-var-nginx-module-${lua_var_nginx_module_ver}
+fi
+
+if [ "$repo" == grpc-client-nginx-module ]; then
+    cp -r "$prev_workdir" ./grpc-client-nginx-module-${grpc_client_nginx_module_ver}
+else
+    git clone --depth=1 -b $grpc_client_nginx_module_ver \
+        https://github.com/api7/grpc-client-nginx-module \
+        grpc-client-nginx-module-${grpc_client_nginx_module_ver}
+fi
+
+cd ngx_multi_upstream_module-${ngx_multi_upstream_module_ver} || exit 1
+./patch.sh ../openresty-${or_ver}
+cd ..
+
+cd apisix-nginx-module-${apisix_nginx_module_ver}/patch || exit 1
+./patch.sh ../../openresty-${or_ver}
+cd ../..
+
+cd wasm-nginx-module-${wasm_nginx_module_ver} || exit 1
+./install-wasmtime.sh
+cd ..
+
+cc_opt=${cc_opt:-}
+ld_opt=${ld_opt:-}
+luajit_xcflags=${luajit_xcflags:="-DLUAJIT_NUMMODE=2 -DLUAJIT_ENABLE_LUA52COMPAT"}
+no_pool_patch=${no_pool_patch:-}
+# TODO: remove old NGX_HTTP_GRPC_CLI_ENGINE_PATH once we have released a new
+# version of grpc-client-nginx-module
+grpc_engine_path="-DNGX_GRPC_CLI_ENGINE_PATH=$OR_PREFIX/libgrpc_engine.so -DNGX_HTTP_GRPC_CLI_ENGINE_PATH=$OR_PREFIX/libgrpc_engine.so"
+
+
+cd openresty-${or_ver} || exit 1
+./configure --prefix="$OR_PREFIX" \
+    --with-cc-opt="-DAPISIX_BASE_VER=$version $grpc_engine_path $cc_opt" \
+    --with-ld-opt="-Wl,-rpath,$OR_PREFIX/wasmtime-c-api/lib $ld_opt" \
+    $debug_args \
+    --add-module=../mod_dubbo-${mod_dubbo_ver} \
+    --add-module=../ngx_multi_upstream_module-${ngx_multi_upstream_module_ver} \
+    --add-module=../apisix-nginx-module-${apisix_nginx_module_ver} \
+    --add-module=../apisix-nginx-module-${apisix_nginx_module_ver}/src/stream \
+    --add-module=../apisix-nginx-module-${apisix_nginx_module_ver}/src/meta \
+    --add-module=../wasm-nginx-module-${wasm_nginx_module_ver} \
+    --add-module=../lua-var-nginx-module-${lua_var_nginx_module_ver} \
+    --add-module=../grpc-client-nginx-module-${grpc_client_nginx_module_ver} \
+    --with-poll_module \
+    --with-pcre-jit \
+    --without-http_rds_json_module \
+    --without-http_rds_csv_module \
+    --without-lua_rds_parser \
+    --with-stream \
+    --with-stream_ssl_module \
+    --with-stream_ssl_preread_module \
+    --with-http_v2_module \
+    --without-mail_pop3_module \
+    --without-mail_imap_module \
+    --without-mail_smtp_module \
+    --with-http_stub_status_module \
+    --with-http_realip_module \
+    --with-http_addition_module \
+    --with-http_auth_request_module \
+    --with-http_secure_link_module \
+    --with-http_random_index_module \
+    --with-http_gzip_static_module \
+    --with-http_sub_module \
+    --with-http_dav_module \
+    --with-http_flv_module \
+    --with-http_mp4_module \
+    --with-http_gunzip_module \
+    --with-threads \
+    --with-compat \
+    --with-luajit-xcflags="$luajit_xcflags" \
+    $no_pool_patch \
+    -j8
+
+make -j8
+sudo make install
+cd ..
+
+cd apisix-nginx-module-${apisix_nginx_module_ver} || exit 1
+sudo OPENRESTY_PREFIX="$OR_PREFIX" make install
+cd ..
+
+cd wasm-nginx-module-${wasm_nginx_module_ver} || exit 1
+sudo OPENRESTY_PREFIX="$OR_PREFIX" make install
+cd ..
+
+cd grpc-client-nginx-module-${grpc_client_nginx_module_ver} || exit 1
+sudo OPENRESTY_PREFIX="$OR_PREFIX" make install
+cd ..
